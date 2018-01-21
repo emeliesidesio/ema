339b89f5077b50c6d7f367e85b0304e3a15f8719
Emelie <esidesio@gmail.com>
2018-01-21 15:14:19 +0100

    Project setup

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express"
+import bodyparser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import uuid from "uuid/v4"
+import bcrypt from "bcrypt-nodejs"
+
+// Express setup, including JSON body parsing.
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Tells express to add the "Access-Control-Allow-Origin" header to allow requests from anywhere.
+app.use(cors())
+
+// Connect to MongoDB, on the "products-api" database. If the db doesn't
+// exist, mongo will create it.
+mongoose.connect("mongodb://localhost/ema", { useMongoClient: true })
+
+// This makes mongo use ES6 promises, instead of its own implementation
+mongoose.Promise = Promise
+
+// Log when mongo connects, or encounters errors when trying to connect.
+mongoose.connection.on("error", err => console.error("Connection error:", err))
+mongoose.connection.once("open", () => console.log("Connected to mongodb"))
+
+//
+// Define a model here.
+//
+
+// Example root endpoint to get started with
+app.get("/", (req, res) => {
+  const password = "supersecretpassword"
+  const hash = bcrypt.hashSync(password)
+
+  // bcrypt.compareSync("supersecretpassword", hash) // true
+  // bcrypt.compareSync("incorrectpassword", hash) // false
+
+  res.send(`ema api. Here's an example of an encrypted password: ${hash}`)
+})
+
+// Add more endpoints here!
+
+app.listen(8080, () => console.log("EMA listening on port 8080!"))
