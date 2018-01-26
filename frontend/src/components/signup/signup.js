afb353eb012852c6f0b924f3eef35706fb2c3d9a
Emelie <esidesio@gmail.com>
2018-01-26 08:12:08 +0100

    Login page styling

--- frontend/src/components/signup/signup.js
+++ frontend/src/components/signup/signup.js
@@ -60,7 +60,6 @@ export default class Signup extends React.Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit} className="signup-form">
-          <p>Not a member yet? Sign up here:</p>
           <div><input type="text" value={this.state.firstName} onChange={this.handleFirstName} required placeholder="First name"/></div>
           <div><input type="text" value={this.state.lastName} onChange={this.handleLastName} required placeholder="Last name" /></div>
           <div><input type="email" value={this.state.email} onChange={this.handleEmail} required placeholder="Email" /></div>
--- frontend/src/pages/account/index.js
+++ frontend/src/pages/account/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Login from "components/login/login"
 import Signup from "components/signup/signup"
+import arrowdown from "assets/down-arrow.png"
 import "./index.css"
 
 export default class Account extends React.Component {
@@ -8,7 +9,12 @@ export default class Account extends React.Component {
   render() {
     return (
       <div className="account-page">
-        <Login />
+        <div className="login-section">
+          <h2>Already an account?</h2>
+          <Login />
+          <img src={arrowdown} alt=""/>
+          <p>Not a member yet? Sign up below</p>
+        </div>
         <Signup />
       </div>
     )
