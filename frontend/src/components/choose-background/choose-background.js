87fdda9e40c91b5ea4eae95d0cf5adec01488331
Emelie <esidesio@gmail.com>
2018-01-26 14:50:26 +0100

    Small uppdates

--- frontend/src/components/choose-background/choose-background.js
+++ frontend/src/components/choose-background/choose-background.js
@@ -5,10 +5,6 @@ export default class ChooseBackground extends React.Component {
 
   render() {
     return (
-      // <label className="background-radio">
-      //   <input type="radio" value={this.props.image} name="background" onChange={this.props.chooseBackground} />
-      //   {this.props.name}
-      // </label>
       <option value={this.props.image}>
       {this.props.name}
       </option>
