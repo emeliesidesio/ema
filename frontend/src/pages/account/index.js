9a4173e8a665a092160cde056dfcb9f9b6dcd26e
AnnikaEkstrom <ekstromannika@hotmail.com>
2018-02-01 17:14:29 +0100

    Removed anchor

--- frontend/src/pages/account/index.js
+++ frontend/src/pages/account/index.js
@@ -12,11 +12,11 @@ export default class Account extends React.Component {
         <div className="login-section">
           <h2>Already have an account?</h2>
           <Login />
-          <a href="#signupAnchor"><img src={arrowdown} alt="" /></a>
+          <img src={arrowdown} alt="" />
           <p>Not a member yet? Sign up below</p>
         </div>
         <div className="signup-section" id="signup">
-          <a name="signupAnchor"><h2>New to Seize the party?</h2></a>
+          <h2>New to Seize the party?</h2>
           <Signup />
         </div>
       </div>
